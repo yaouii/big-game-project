8451a63eb96425c93ecbe6c20d566ccd8559fd0e
Ruby Lee <Le6579Ru0208@pusd.us>
2024-10-10 10:47:51 -0700

    new project

---
+++ objects/animated_sprite_2d.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite2D
+
+func _process(delta):
+	# play animation while right cursor key is pressed
+	if Input.is_action_pressed("ui_down"):
+		play("walk_forward")
+		
+	elif Input.is_action_pressed("ui_up"):
+		play("walk_backwards")
+		
+	elif Input.is_action_pressed("ui_right"):
+		play("walk_right")
+	
+	elif Input.is_action_pressed("ui_left"):
+		play("walk_left")
+	else:
+		stop()
